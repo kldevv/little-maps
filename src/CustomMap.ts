6adcf5317839622a306928c4dfc313f9087eee2c
Kuan-You Lin <linstudyhard1997@gmail.com>
2021-11-02 18:00:45 -0400

    Create CustomMap to restrict the usage of api of google.maps and add Marker and generic infoWindow

---
+++ src/CustomMap.ts
@@ -0,0 +1,42 @@
+// Instructions to every other class
+// on how they can be argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number,
+    lng: number
+  };
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(elementDivId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(elementDivId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  addMaker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener('click', ()=> {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there!'
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 console.log(user);
@@ -7,10 +8,6 @@ console.log(user);
 const company = new Company();
 console.log(company);
 
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
+const customMap = new CustomMap('map');
+customMap.addMaker(user);
+customMap.addMaker(company);
